8ec8a0027ec588f2c7e18f1a0c2fd0f91c910e3c
Promise-Digitals <promiseedeh025@gmail.com>
2024-09-03 22:06:44 +0100

    Lesson 3

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
 
 const Home = () => {
   return (
     <div className="home container">
-      <h2>
+      <motion.h2 animate={{}}>
         Welcome to Pizza Joint
-      </h2>
+      </motion.h2>
       <Link to="/base">
-        <button>
+        <motion.button
+          animate={{}}
+        >
           Create Your Pizza
-        </button>
+        </motion.button>
       </Link>
     </div>
   )
