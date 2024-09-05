6984eaeb3d556dcd78e2f1ee88ffcd0f83f229b8
Promise-Digitals <promiseedeh025@gmail.com>
2024-09-05 21:39:25 +0100

    Lesson 9

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,13 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { motion } from 'framer-motion';
 
+
+const buttonVariants = {
+  hover: {
+    scale: [1, 1.1, 1, 1.1, 1, 1.1, 1], textShadow: '0 0 8px rgb(255, 255, 255)', boxShadow: '0 0 8px rgb(255, 255, 255)'
+  }
+}
+
 const Home = () => {
   return (
     <motion.div className="home container" initial={{opacity: 0}} animate={{opacity: 1}} transition={{delay: 1.5, duration: 1}}>
@@ -10,9 +17,7 @@ const Home = () => {
         Welcome to Pizza Joint
       </motion.h2>
       <Link to="/base">
-        <motion.button
-          whileHover={{ scale: 1.1, textShadow: '0 0 8px rgb(255, 255, 255)', boxShadow: '0 0 8px rgb(255, 255, 255)' }}
-        >
+        <motion.button variants={buttonVariants} whileHover="hover">
           Create Your Pizza
         </motion.button>
       </Link>
