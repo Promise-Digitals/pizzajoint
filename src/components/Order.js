bedbb935cb915349070019d0d81d8fdd3a43c710
Promise-Digitals <promiseedeh025@gmail.com>
2024-09-06 21:17:57 +0100

    Lesson 10

--- src/components/Order.js
+++ src/components/Order.js
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { motion, stagger } from 'framer-motion';
+import { AnimatePresence } from 'framer-motion';
 
 const Order = ({ pizza }) => {
+
+  const [showTitle, setShowTitle] = useState(true);
+
+  setTimeout(()=>{
+    setShowTitle(false)
+  }, 4000)
 
   const containerVariants = {
     hidden: {
@@ -26,7 +33,14 @@ const Order = ({ pizza }) => {
 
   return (
     <motion.div className="container order" variants={containerVariants} initial="hidden" animate="visible">
-      <h2>Thank you for your order :)</h2>
+
+      <AnimatePresence>
+        {showTitle && (
+          <motion.h2 exit={{y: -1000}}>Thank you for your order :</motion.h2>
+        )}  
+      </AnimatePresence>
+      
+      
       <motion.p variants={childVariants}>You ordered a {pizza.base} pizza with:</motion.p>
       <motion.div variants={childVariants}>
         {pizza.toppings.map(topping => <div key={topping}>{topping}</div>)}
